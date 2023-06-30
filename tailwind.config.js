077530172d677b450cc78c9a48d549cb0a058f6c
Neo Kefilwe Gaboeme <neo@morofin.com>
2023-06-30 18:01:34 +0200

    updated fonts and image column height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
      darkMode: false, // or 'media' or 'class'
      theme: {
-     },
+      fontFamily: {
+        'sans': ['Helvetica', 'system-ui'],
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    },
      variants: {
        extend: {},
      },
